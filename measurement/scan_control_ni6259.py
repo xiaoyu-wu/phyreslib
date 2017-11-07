6d95b4df2c4015ce87db24f6c7cc5b349b20ea04
Xiaoyu Wu <xywu@utexas.edu>
2017-11-07 11:56:21 -0600

    Added scan with rotation, half done.

--- measurement/scan_control_ni6259.py
+++ measurement/scan_control_ni6259.py
@@ -80,20 +80,37 @@ def generate_piezo_voltage(min_volt, max_volt, scan_rate, pixels, waveform="Tria
     return generate_waveform(waveform=waveform, time_span=time_span, freq=scan_rate, samp_rate=samp_rate, amplitude=amplitude, phase=phase, offset=offset)
 
 
-def averageArray(input_array, averaging_points):
-    if type(input_array) != np.ndarray:
-        print "Input array must be numpy array type!"
+def generate_output_voltage(scan_range, scan_rate, pixels, rotation=0, fast_axis='x', waveform='Triangular'):
+    """ Generates output voltage sequences to be applied to x & y channels
+
+    Parameters
+    ----------
+    scan_range: float
+        range to scan in volts
+    rotation: float
+        counterclockwise angle in rad from original x
+    fast_axis: x
+        choose from x and y
+
+    Returns
+    -------
+    Tuple of numpy arrays
+    """
+    if fast_axis == 'x':
+        fast_voltages = generate_piezo_voltage(0, scan_range * np.cos(rotation), scan_rate, pixels, waveform=waveform)
+        slow_voltages = generate_piezo_voltage(0, scan_range * np.sin(rotation), scan_rate, pixels, waveform=waveform)
+    elif fast_axis == 'y':
+        slow_voltages = generate_piezo_voltage(0, -scan_range * np.sin(rotation), scan_rate, pixels, waveform=waveform)
+        fast_voltages = generate_piezo_voltage(0, scan_range * np.cos(rotation), scan_rate, pixels, waveform=waveform)
     else:
-        input_array_length = input_array.size
-        output_array_length = input_array_length // averaging_points
-        output_array = np.zeros(output_array_length, dtype=np.float64)
-        for j in range(output_array_length):
-            output_array[j] = np.average(input_array[averaging_points*j:averaging_points*(j+1):1])
-        return output_array
+        raise ValueError("Invalid fast axis selection. Must be \"x\" or \"y\"")
+        return
+    return fast_voltages, slow_voltages
 
 
 AO_PORTS = range(4)
 AI_PORTS = range(24)
+
 
 class ScanController(HasTraits):
     """ Controls voltages to be supplied scanners
@@ -102,50 +119,91 @@ class ScanController(HasTraits):
     x_axis_index = Enum(AO_PORTS)
     y_axis_index = Enum(AO_PORTS)
     scan_rate = Float(0.1)
-    x_scan_range = Tuple((0, 2))
-    y_scan_range = Tuple((0, 2))
+    fast_scan_range = Float(1)
+    slow_scan_range = Float(1)
     pixels = Int(256)
     output_smoothing_ratio = 100
     input_averaging_pts = 100
-
+    rotation = Float(0)
+    fast_axis = Enum("x", "y")
+    x_start = Float(0)
+    y_start = Float(0)
 
     def __init__(self, device_name, x_axis_index=0, y_axis_index=1):
         self.daq_board = NI6259(device_name)
         self.x_axis_index = x_axis_index
         self.y_axis_index = y_axis_index
+        self.fast_axis = "x"
+
+    def set_axis_voltage(self):
+        pass
 
     def sync_scan_read_1d(self):
         samp_time = 1.0 / self.scan_rate
         output_samples = self.pixels * self.output_smoothing_ratio * 2  # 2 for trace and retrace
         input_samples = self.pixels * self.input_averaging_pts * 2      # 2 for trace and retrace
-        ao_data = generate_piezo_voltage(self.x_scan_range[0], self.x_scan_range[1], self.scan_rate, output_samples / 2)
+        ao_data = generate_piezo_voltage(0, self.fast_scan_range, self.scan_rate, output_samples / 2)
         output_samp_rate = (output_samples) / samp_time
         input_samp_rate = (input_samples) / samp_time
         t_data, ai_data = self.daq_board.sync_aoai(self.x_axis_index, 1, ao_data, samp_time, output_samp_rate, input_samp_rate)
-        # ai_data_avg = averageArray(ai_data, self.input_averaging_pts)
+        # averages the input sequence every n points, n = input_averaging_pts
+        ai_data_avg = np.mean(ai_data.reshape(-1, self.input_averaging_pts), axis=1)
+        return ai_data_avg
+
+    def sync_scan_read_1d_with_rotation(self):
+        samp_time = 1.0 / self.scan_rate
+        output_samples = self.pixels * self.output_smoothing_ratio * 2  # 2 for trace and retrace
+        input_samples = self.pixels * self.input_averaging_pts * 2      # 2 for trace and retrace
+        fast_ao_data, slow_ao_data = generate_output_voltage(self.fast_scan_range, self.scan_rate,
+                                                             output_samples / 2, rotation=self.rotation,
+                                                             fast_axis=self.fast_axis)
+        fast_ao_data += self.x_start
+        output_samp_rate = output_samples / samp_time
+        input_samp_rate = input_samples / samp_time
+        t_data, ai_data = self.daq_board.sync_aoai(self.x_axis_index, 1, fast_ao_data, samp_time, output_samp_rate, input_samp_rate)
         ai_data_avg = np.mean(ai_data.reshape(-1, self.input_averaging_pts), axis=1)
         return ai_data_avg
 
     def sync_scan_read_2d(self):
-        slow_axis_squence = np.linspace(self.y_scan_range[0], self.y_scan_range[1], self.pixels)
-        image_data = np.zeros((self.pixels, self.pixels))
+        slow_axis_sequence = np.linspace(0, self.slow_scan_range, self.pixels)
+        image_data = np.zeros((self.pixels, 2 * self.pixels))
+
         for i in range(self.pixels):
-            self.daq_board.set_ao(self.y_axis_index, slow_axis_squence[i])
+            self.daq_board.set_ao(self.y_axis_index, slow_axis_sequence[i])
             ai = self.sync_scan_read_1d()
-            image_data[i] = ai[:self.pixels]
-        return image_data
+            image_data[i] = ai[:]
+        image_data_trace = image_data[:, :self.pixels]
+        image_data_retrace = image_data[:, self.pixels:]
+        return image_data_trace, image_data_retrace
+
+    def sync_scan_read_2d_with_rotation(self):
+        slow_axis_sequence = np.linspace(0, self.slow_scan_range, self.pixels)
+        image_data = np.zeros((self.pixels, 2 * self.pixels))
+
+        for i in range(self.pixels):
+            self.daq_board.set_ao(self.y_axis_index, slow_axis_sequence[i])
+            ai = self.sync_scan_read_1d_with_rotation()
+            image_data[i] = ai[:]
+        image_data_trace = image_data[:, :self.pixels]
+        image_data_retrace = image_data[:, self.pixels:]
+        return image_data_trace, image_data_retrace
 
 
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
     test = ScanController('NI6259')
-    test.scan_rate = 0.4
-    test.pixels = 64
+    test.scan_rate = 1
+    test.pixels = 32
+    test.rotation = np.pi / 4.0
+    test.x_start = 1
     # ao_data, ai_data = test.sync_scan_read_1d()
     # plt.plot(ao_data, 'b-')
     # plt.plot(ai_data, 'r-')
     # plt.show()
     # print ao_data[-10:-1], ai_data[-10:-1]
-    image = test.sync_scan_read_2d()
-    plt.matshow(image)
+    image_t, image_r = test.sync_scan_read_2d_with_rotation()
+    plt.subplot(121)
+    plt.imshow(image_t)
+    plt.subplot(122)
+    plt.imshow(image_r)
     plt.show()
